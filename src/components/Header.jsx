1f2d0162056c124757e9c071f2966a9220e69fa4
henrydaniels1 <henrydanielchibuzor@gmail.com>
2024-08-11 11:58:53 +0100

    final8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,28 @@
 import enc from "/enc.jpg"
 import { Link } from 'react-router-dom';
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 
 
 export const Header = () =>
 {
   return (
-      <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      viewport={{ once: true }}
-    >
+    //   <motion.div
+    //   initial={{ opacity: 0, y: 50 }}
+    //   whileInView={{ opacity: 1, y: 0 }}
+    //   transition={{ duration: 0.5 }}
+    //   viewport={{ once: true }}
+    // >
+    <>
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
       <div className="flex flex-col items-center justify-between lg:flex-row">
         <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
           <div className="max-w-xl mb-6">
-            {/* <div>
-              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
-                Brand new
-              </p>
-            </div> */}
+           
             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
              Secure Your Data with Our
               <br className="hidden md:block" />
-              {/* jumps over{' '} */}
+             
               <span className="inline-block text-deep-purple-accent-400">
                Encryption and Decryption Tool
               </span>
@@ -35,14 +32,7 @@ export const Header = () =>
             </p>
           </div>
           <div className="flex flex-col items-center md:flex-row">
-            {/* <a
-              href="/"
-                          className="inline-flex items-center justify-center w-full p-4 mb-3 font-medium 
-                tracking-wide text-white transition duration-200 rounded-lg shadow-md md:w-auto md:mr-4 md:mb-0 bg-purple-600 hover:bg-purple-700 focus:shadow-outline focus:outline-none"
-            >
-              <span className="">Get Started</span>
-              
-            </a> */}
+           
             <div className="inline-flex items-center justify-center w-full p-4 mb-3 font-medium
                 tracking-wide text-white transition duration-200 rounded-lg md:w-auto
                  md:mr-4 md:mb-0 bg-purple-600 hover:bg-purple-700 focus:shadow-outline focus:outline-none">
@@ -62,6 +52,8 @@ export const Header = () =>
             alt=""/>
         </div>
       </div>
-    </div> </motion.div>
+    </div>
+    </>
+    // </motion.div>
   );
 };
