8ae6a6a870c4965608a5f51b67712a56b32bbfbe
henrydaniels1 <henrydanielchibuzor@gmail.com>
2024-08-11 11:05:03 +0100

    final6

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -214,7 +214,7 @@ export const Footer = () => {
         </div>
         <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
           <p className="text-sm text-gray-500">
-            © Copyright 2020 Henry-Daniel. All rights reserved.
+            © Copyright 2024 Henry-Daniel. All rights reserved.
           </p>
           <div className="flex items-center mt-4 space-x-4 sm:mt-0">
             <a
